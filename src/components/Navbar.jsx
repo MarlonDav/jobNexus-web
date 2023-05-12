e2f959c13b597bfc663f657a96ae8cca7beb6732
MarlonDav <marlon.bernal.riveros@gmail.com>
2023-05-12 11:10:09 -0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='bg-cyan-300 flex justify-center '>
+        <diV className='flex items-center'>
+            <ul>
+                <li>Inicio</li>
+                <li>contactos</li>
+                <li>perfil</li>
+                <li>Login </li>
+                <li>Registro</li>
+            </ul>
+        </diV>
+    </div>
+  )
+}
+
+export default Navbar
