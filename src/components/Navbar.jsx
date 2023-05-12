e35fc5dab96606ca672e338bd2da1c7a825335e1
MarlonDav <marlon.bernal.riveros@gmail.com>
2023-05-12 11:46:04 -0500

    papasito

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='bg-cyan-300 flex justify-center '>
-        <diV className='flex items-center'>
-            <ul>
+    <div className='bg-cyan-300 flex justify-center text-black h-[60px] w-full py-2 px-6 '>
+        <diV className=' h-full w-10/12 my-auto flex jutify-between items-center'>
+            <ul className='flex justify-center items-center gap-6 text-[#efefef] text-[18px]'>
                 <li>Inicio</li>
                 <li>contactos</li>
                 <li>perfil</li>
